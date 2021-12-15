ed60916b9464489a8ca883653013ccf81c31170b
fdsecurity <info.fdseguridadinformatica@gmail.com>
2021-12-15 19:02:55 -0300

    fix errors and imgs

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,12 +1,12 @@
 import {Link} from 'react-router-dom';
 
-const loadImage = (imageName) => (require(`../../assets/images/${imageName}`).default);
+// const loadImage = (imageName) => (require(`../../assets/images/${imageName}`).default);
 
 const Item = ({product}) => {
 
   return (
     <div className="cardProduct"  key={product.id}>
-      <img src={loadImage(`${product.image}`)} alt="Imagen de producto" className="imgCardProduct"/>
+      {/* <img src={loadImage(`${product.image}`)} alt="Imagen de producto" className="imgCardProduct"/> */}
       <h3 className="nameProduct">{product.name}</h3>
       <h3 className="priceProduct">$ {product.price}</h3>
       <Link to={`/details/${product.id}`}><button className='btnVerMas' >Detalles</button></Link>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,12 @@
 import ItemCount from "../ItemCount/ItemCount";
 
-const loadImage = (imageName) => (require(`../../assets/images/${imageName}`).default);
+// const loadImage = (imageName) => (require(`../../assets/images/${imageName}`).default);
 
 const ItemDetail = ({product}) => {
   return (
     <div className="itemDetail" key={product.id}>
       <h2 className="nameDetailProduct">{product.name}</h2>
-      <img src={loadImage(`${product.image}`)} alt="Imagen detalle de producto" className="imgDetailProduct"/>
+      {/* <img src={loadImage(`${product.image}`)} alt="Imagen detalle de producto" className="imgDetailProduct"/> */}
       <p className="priceDetailProduct">Precio: ${product.price}</p>
       <p className="descriptionDetailProduct">{product.description}</p>
       <ItemCount stock={product.stock} initial={1}/>
